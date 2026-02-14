7b074dfb642e6dbcc3845836324c6465a88ae221
natascha <natascha@i-comp.de>
2026-02-14 10:54:31 +0100

    add basic html structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{ position: 'relative' }}>
+
+      <div>
+        <button className="close-button">Close</button>
+      </div>
+
+      <div className="home-content">
+        <div className="home-controls">
+          <button className="image-button">Concentrate</button>
+          <button className="image-button">Take break</button>
+        </div>
+
+        <p>Tschakka!</p>
+        <h1 className="home-timer">25:00</h1>
+        <button className="home-button">Start</button>
+      </div>
+    </div >
   );
 }
 
